6e7ddbde74b2b572927437bba9b6f0a2e8217551
Remy Tuyeras <rtuyeras@gmail.com>
2025-05-13 12:12:00 -0400

    added test import for hello_summoner

--- tooling/your_package/hellosummoner.py
+++ tooling/your_package/hellosummoner.py
@@ -3,5 +3,7 @@ target_path = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__fil
 if target_path not in sys.path:
     sys.path.insert(0, target_path)
 
+# from summoner.client import SummonerClient
+
 def hello_summoner():
     print("Hello Summoner!")
